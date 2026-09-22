04ce3084021327449cf60922faa4031c8ae763db
S H Mahammed Saleeth <selith514@gmail.com>
2026-09-22 21:59:14 +0530

    Change greeting from 'Hello' to 'Goodbye'

--- Hello.java
+++ Hello.java
@@ -1869,3 +1869,18 @@ public class GroceryStoreBilling {
         sc.close();
     }
 }
+
+int original = x;
+int reverse = 0;
+
+while (x > 0) {
+    int digit = x % 10;
+    reverse = reverse * 10 + digit;
+    x = x / 10;
+}
+
+if (original == reverse) {
+    return true;
+} else {
+    return false;
+}
